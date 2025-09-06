725ca4b6d82dc2c1f09da5644116f80fa2cf00fd
unknown <Kamaleshwar279@gmail.com>
2025-09-06 19:09:38 +0530

    Add python-dotenv to requiremed

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 )
 
 
-GEMINI_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_API_KEY
 
 
